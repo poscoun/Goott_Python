e26eb04f0561e50d4ece266895feebd191ac5426
JYJ <poscoun@gmail.com>
2021-04-14 17:18:33 +0900

    2021-04-14

--- list/listEx02.py
+++ list/listEx02.py
@@ -34,3 +34,42 @@ c = [10, 20, 30, 40, 50]
 print(c.index(40))  # 위치값이 인덱스 번호임에 유의
 # print(c.index(60))  # 찾는 값이 없으면 error
 
+# 리스트에 요소 삽입 : insert
+# insert(x, y) : x번째 위치에 y 값을 삽입
+c.insert(3, 4)
+print(c)
+
+# 리스트 요소 제거 : remove
+c.remove(4)
+print(c)
+
+c.insert(3, 40)
+print(c)
+
+c.remove(40)    # 중복되는 값이 있으면 첫번째 요소만 삭제
+print(c)
+
+# 리스트 요소 꺼내기 - pop
+d = [1, 2, 3]
+d.pop()
+print(d)
+
+# pop(x) : x번째 요소를 삭제
+e = [1, 2, 3, 4, 5]
+e.pop(3)
+print(e)
+
+# 리스트에 포함된 요소의 갯수 세기 : count
+f = [10, 20, 20, 30, 30, 30]
+print(f.count(30))
+
+# 리스트 확장 : extend
+g = [1, 2, 3]
+g.extend([4, 5])
+print(g)
+g.extend([6, 7])
+print(g)
+
+g += [8, 9]
+print(g)
+
