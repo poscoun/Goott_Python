2e988414d6847717ccd2d4e47bdf10222d027d45
JYJ <poscoun@gmail.com>
2021-04-16 16:50:15 +0900

    del

--- except/exceptionObject.py
+++
@@ -1,9 +0,0 @@
-print()
-'''
-# 예외의 최상위 객체 : Exception
-# 사용자 정의를 위한 새 예외를 만들 때 Exception 클래스를 상속해도 되고,
-  흔한 오류를 처리할 경우라면 ValueError를 상속해도 됨
-'''
-class Unexpected_exep(Exception):
-    pass
-    print('새 예외')
